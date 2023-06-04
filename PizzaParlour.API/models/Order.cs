e407ec6cd62f8b87406979769adb9739a6da4d11
vish <mvishnurana@gmail.com>
2023-06-05 04:36:51 +0800

    refactored Customer and Order model

--- PizzaParlour.API/models/Order.cs
+++ PizzaParlour.API/models/Order.cs
@@ -3,9 +3,17 @@
     public class Order
     {
         public int Id { get; set; }
+        public int CustomerID { get; set; }
         public DateTime OrderDateTime { get; set; }
         public List<Pizza> Pizzas { get; set; } = new List<Pizza>();
         public string? Instructions { get; set; }
         public double Price { get; set; }
+        public OrderType OrderType { get; set; }
+    }
+
+    public enum OrderType
+    {
+        PickUp,
+        Delivery
     }
 }
